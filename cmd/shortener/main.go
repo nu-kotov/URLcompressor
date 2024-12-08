d9e7f732fab9b4f138de1043fe1e3322f390e153
Никита <8.complexity@gmail.com>
2024-12-08 16:33:53 +0300

    Роутер компрессора ссылок

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,64 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"math/rand"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func (uc *URLCompressor) postCompressURLHandler(res http.ResponseWriter, req *http.Request) {
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		http.Error(res, "Invalid body", http.StatusBadRequest)
+		return
+	}
+	shortKey := generateShortKey()
+	uc.urls[shortKey] = string(body)
+	res.Header().Set("content-type", "text/plain")
+	res.WriteHeader(http.StatusCreated)
+	io.WriteString(res, string(shortKey))
+}
+
+func (uc *URLCompressor) getShortURLById(res http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	shortUrlId := params["id"]
+	originalURL, found := uc.urls[shortUrlId]
+	if !found {
+		http.Error(res, "Shortened key not found", http.StatusBadRequest)
+		return
+	}
+	res.WriteHeader(http.StatusTemporaryRedirect)
+	io.WriteString(res, originalURL)
+}
+
+func generateShortKey() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const keyLength = 6
+
+	shortKey := make([]byte, keyLength)
+	for i := range shortKey {
+		shortKey[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(shortKey)
+}
+
+type URLCompressor struct {
+	urls map[string]string
+}
+
+func main() {
+	urlCompressor := &URLCompressor{
+		urls: make(map[string]string),
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc(`/`, urlCompressor.postCompressURLHandler).Methods("POST")
+	router.HandleFunc(`/{id:\w+}`, urlCompressor.getShortURLById).Methods("GET")
+
+	err := http.ListenAndServe(`:8080`, router)
+	if err != nil {
+		panic(err)
+	}
+}
