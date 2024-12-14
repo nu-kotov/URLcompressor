10c36e8236bcee1f1470a8f5661980e11cd4882b
Никита <8.complexity@gmail.com>
2024-12-15 02:24:54 +0300

    Алгоритм сокращения ссылки через хеш.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,55 +1,74 @@
 package main
 
 import (
+	"hash/fnv"
 	"io"
-	"math/rand"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/sqids/sqids-go"
 )
 
+func hash(bytes []byte) uint64 {
+
+	h := fnv.New64a()
+	h.Write([]byte(bytes))
+
+	return h.Sum64()
+}
+
 func CompressURLHandler(res http.ResponseWriter, req *http.Request) {
+
 	if req.Method == http.MethodPost {
-		body, err := io.ReadAll(req.Body)
-		if err != nil {
+
+		body, readingBodyError := io.ReadAll(req.Body)
+		if readingBodyError != nil {
 			http.Error(res, "Invalid body", http.StatusBadRequest)
 			return
 		}
-		shortKey := generateShortKey()
-		savedURLs[shortKey] = string(body)
+
+		sqids, newSqidsError := sqids.New()
+		if newSqidsError != nil {
+			http.Error(res, "Sqids error", http.StatusBadRequest)
+			return
+		}
+
+		bodyHash := hash(body)
+		shortID, IDCreatingError := sqids.Encode([]uint64{bodyHash})
+		if IDCreatingError != nil {
+			http.Error(res, "IDCreatingError error", http.StatusBadRequest)
+			return
+		}
+
+		savedURLs[shortID] = string(body)
+
 		res.Header().Set("Content-Type", "text/plain")
 		res.WriteHeader(http.StatusCreated)
-		io.WriteString(res, string("http://"+req.Host+"/"+shortKey))
+		io.WriteString(res, string(req.URL.Scheme+req.Host+"/"+shortID))
+
 	} else {
 		res.WriteHeader(http.StatusBadRequest)
 	}
 }
 
 func ShortURLByID(res http.ResponseWriter, req *http.Request) {
+
 	if req.Method == http.MethodGet {
+
 		params := mux.Vars(req)
 		shortURLID := params["id"]
-		if originalURL, ok := savedURLs[shortURLID]; ok {
+
+		if originalURL, exists := savedURLs[shortURLID]; exists {
 			res.Header().Set("Location", originalURL)
 			res.WriteHeader(http.StatusTemporaryRedirect)
 		} else {
 			res.WriteHeader(http.StatusBadRequest)
 		}
+
 	} else {
 		res.WriteHeader(http.StatusBadRequest)
 	}
 
-}
-
-func generateShortKey() string {
-	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	const keyLength = 6
-
-	shortKey := make([]byte, keyLength)
-	for i := range shortKey {
-		shortKey[i] = charset[rand.Intn(len(charset))]
-	}
-	return string(shortKey)
 }
 
 var savedURLs = make(map[string]string)
@@ -59,8 +78,8 @@ func main() {
 	router.HandleFunc(`/`, CompressURLHandler)
 	router.HandleFunc(`/{id:\w+}`, ShortURLByID)
 
-	err := http.ListenAndServe(`:8080`, router)
-	if err != nil {
-		panic(err)
+	listenAndServeError := http.ListenAndServe(`:8080`, router)
+	if listenAndServeError != nil {
+		panic(listenAndServeError)
 	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.22.3
 
 require github.com/gorilla/mux v1.8.1
 
+require github.com/sqids/sqids-go v0.4.1
+
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
