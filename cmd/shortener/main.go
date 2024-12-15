221b937308df7f005ce24097c61a537d996a2ff0
Никита <8.complexity@gmail.com>
2024-12-15 18:10:53 +0300

    Рефакторинг.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -44,7 +44,7 @@ func CompressURLHandler(res http.ResponseWriter, req *http.Request) {
 
 		res.Header().Set("Content-Type", "text/plain")
 		res.WriteHeader(http.StatusCreated)
-		io.WriteString(res, string(req.URL.Scheme+req.Host+"/"+shortID))
+		io.WriteString(res, string("http://"+req.Host+"/"+shortID))
 
 	} else {
 		res.WriteHeader(http.StatusBadRequest)
