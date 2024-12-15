a3fd8b6e4a93d3552fbd16a8a913e2fa9c291aa3
Никита <8.complexity@gmail.com>
2024-12-15 15:35:54 +0300

    Тест ручки создания короткой ссылки с запросом к серверу.

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -3,16 +3,34 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/go-resty/resty/v2"
+	"github.com/sqids/sqids-go"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCompressURLHandler(t *testing.T) {
+
+	handler := http.HandlerFunc(CompressURLHandler)
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	testURL := []byte("http://ya.ru")
+	URLHash := hash(testURL)
+
+	sqids, newSqidsError := sqids.New()
+	assert.NoError(t, newSqidsError, "error making sqids variable")
+
+	shortID, IDCreatingError := sqids.Encode([]uint64{URLHash})
+	assert.NoError(t, IDCreatingError, "error making %v expected variable", shortID)
+
 	type want struct {
-		code        int
-		contentType string
-		method      string
+		statusCode      int
+		contentType     string
+		method          string
+		expectedShortID string
 	}
 	tests := []struct {
 		name string
@@ -21,48 +39,67 @@ func TestCompressURLHandler(t *testing.T) {
 		{
 			name: "Creating short url",
 			want: want{
-				code:        201,
-				contentType: "text/plain",
-				method:      http.MethodPost,
+				statusCode:      201,
+				contentType:     "text/plain",
+				method:          http.MethodPost,
+				expectedShortID: shortID,
 			},
 		},
 		{
 			name: "Wrong method - resp status 400",
 			want: want{
-				code:        400,
-				contentType: "",
-				method:      http.MethodGet,
+				statusCode:      400,
+				contentType:     "",
+				method:          http.MethodGet,
+				expectedShortID: "",
 			},
 		},
 		{
 			name: "Wrong method - resp status 400",
 			want: want{
-				code:        400,
-				contentType: "",
-				method:      http.MethodPut,
+				statusCode:      400,
+				contentType:     "",
+				method:          http.MethodPut,
+				expectedShortID: "",
 			},
 		},
 		{
 			name: "Wrong method - resp status 400",
 			want: want{
-				code:        400,
-				contentType: "",
-				method:      http.MethodDelete,
+				statusCode:      400,
+				contentType:     "",
+				method:          http.MethodDelete,
+				expectedShortID: "",
+			},
+		},
+		{
+			name: "Wrong method - resp status 400",
+			want: want{
+				statusCode:      400,
+				contentType:     "",
+				method:          http.MethodPatch,
+				expectedShortID: "",
 			},
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			request := httptest.NewRequest(test.want.method, "/", nil)
-			w := httptest.NewRecorder()
-			CompressURLHandler(w, request)
 
-			res := w.Result()
-			assert.Equal(t, test.want.code, res.StatusCode)
-			defer res.Body.Close()
+			req := resty.New().R()
+			req.Method = test.want.method
+			req.URL = server.URL
+			req.Body = "http://ya.ru"
+
+			resp, err := req.Send()
+			assert.NoError(t, err, "error making HTTP request")
+
+			assert.Equal(t, test.want.statusCode, resp.StatusCode(), "Response statusCode didn't match expected")
 
 			if test.want.contentType != "" {
-				assert.Equal(t, test.want.contentType, res.Header.Get("Content-Type"))
+				assert.Equal(t, test.want.contentType, resp.Header()["Content-Type"][0])
+			}
+			if test.want.expectedShortID != "" {
+				assert.True(t, strings.HasSuffix(string(resp.Body()), test.want.expectedShortID))
 			}
 		})
 	}
--- go.mod
+++ go.mod
@@ -4,7 +4,12 @@ go 1.22.3
 
 require github.com/gorilla/mux v1.8.1
 
-require github.com/sqids/sqids-go v0.4.1
+require (
+	github.com/go-resty/resty/v2 v2.16.2
+	github.com/sqids/sqids-go v0.4.1
+)
+
+require golang.org/x/net v0.32.0 // indirect
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
