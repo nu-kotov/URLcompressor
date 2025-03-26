9a6d172284570fa972388dac209e026d7c3e31a0
Никита <8.complexity@gmail.com>
2025-03-27 01:52:13 +0300

    Правка.

--- internal/app/middleware/middleware.go
+++ internal/app/middleware/middleware.go
@@ -96,7 +96,7 @@ func RequestCompressor(h http.HandlerFunc) http.HandlerFunc {
 func RequestSession(h http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		_, err := r.Cookie("token")
-		if err != nil && r.Method == http.MethodPost {
+		if err != nil {
 			value, err := auth.BuildJWTString()
 			if err != nil {
 				w.WriteHeader(http.StatusBadRequest)
