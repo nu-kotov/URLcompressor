e25dfc851ecd14bcb652579d2c8d017e45b6ad6c
Никита <8.complexity@gmail.com>
2025-08-03 00:39:17 +0300

    gracefull shutdown

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/nu-kotov/URLcompressor/config"
 	"github.com/nu-kotov/URLcompressor/internal/app/api/handler"
 	"github.com/nu-kotov/URLcompressor/internal/app/logger"
 	"github.com/nu-kotov/URLcompressor/internal/app/storage"
+	"go.uber.org/zap"
 	"golang.org/x/crypto/acme/autocert"
 )
 
@@ -22,6 +28,9 @@ var (
 func main() {
 
 	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n", buildVersion, buildDate, buildCommit)
+
+	sigForShutdown := make(chan os.Signal, 1)
+	signal.Notify(sigForShutdown, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 
 	if err := logger.NewLogger("info"); err != nil {
 		log.Fatal("Error initialize zap logger: ", err)
@@ -44,19 +53,38 @@ func main() {
 		log.Println(http.ListenAndServe("localhost:6060", nil))
 	}()
 
+	server := &http.Server{
+		Addr:    config.RunAddr,
+		Handler: router,
+	}
+
 	if config.EnableHTTPS {
 		manager := &autocert.Manager{
 			Cache:      autocert.DirCache("cache-dir"),
 			Prompt:     autocert.AcceptTOS,
 			HostPolicy: autocert.HostWhitelist("urlcompressor.ru"),
 		}
-		server := &http.Server{
-			Addr:      config.RunAddr,
-			Handler:   router,
-			TLSConfig: manager.TLSConfig(),
-		}
+		server.TLSConfig = manager.TLSConfig()
 		log.Fatal(server.ListenAndServeTLS("", ""))
 	} else {
-		log.Fatal(http.ListenAndServe(config.RunAddr, router))
+		log.Fatal(server.ListenAndServe())
+	}
+
+	<-sigForShutdown
+
+	logger.Log.Info("shutdown signal received...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	if err := service.Storage.Close(); err != nil {
+		logger.Log.Error("Error closing store", zap.Error(err))
+	}
+
+	if err := server.Shutdown(ctx); err != nil {
+		logger.Log.Error("Server forced to shutdown", zap.Error(err))
+	} else {
+		logger.Log.Info("Server shutdown gracefully")
 	}
 }
